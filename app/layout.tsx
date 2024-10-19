f92f339ab9ca087e36a1840fa3298440b53249ce
Sekou Samassa <sksamassa@gmail.com>
2024-10-19 10:55:04 +0500

    Initial commit from Create Next App

---
+++ app/layout.tsx
@@ -0,0 +1,23 @@
+import type { Metadata } from "next";
+import "./globals.css";
+
+export const metadata: Metadata = {
+  title: "Task Management System",
+  description: "Финальаный проект Проектирования и разработки распределенных программных систем",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
