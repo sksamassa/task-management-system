d0e494521a85edab4f20e2bb9146d9d6030ea93a
Sekou Samassa <sksamassa@gmail.com>
2024-10-23 14:59:01 +0500

    implement authentication and change password

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Toaster } from "@/components/ui/toaster";
 
 export const metadata: Metadata = {
   title: "Task Management System",
@@ -16,6 +17,7 @@ export default function RootLayout({
     <html lang="en">
       <body>
         <main>{children}</main>
+        <Toaster />
       </body>
     </html>
   );
--- db/schemas/usersSchema.ts
+++ db/schemas/usersSchema.ts
@@ -9,7 +9,7 @@ import {
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
   email: varchar("email").unique(),
-  password: varchar("email"),
+  password: varchar("password"),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").$onUpdateFn(() => new Date()),
   twoFactorActivated: boolean("2fa_activated"),
